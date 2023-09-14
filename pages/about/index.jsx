47b129087ec7ac7f29a4d073e00b5e97f4f4367a
Omar-H-Assaf <omar.assaf@hearst.com>
2023-09-13 23:49:17 -0500

    add new image file

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -6,11 +6,8 @@ export default function About() {
     return (
         <AboutContainer id="about">
             <AboutBorder>
-                {/* <NameLabel style={{ color: '#333333' }}>
-                    About Me
-                </NameLabel> */}
                 <Aboutcontentcontainer>
-                    <Image src="/omar-icon.png" width={300} height={250} style={{ paddingRight: '3rem' }} />
+                    <Image src="/static/images/omar-icon.png" width={300} height={250} style={{ paddingRight: '3rem' }} />
                     <ContentLabel style={{ color: '#333333' }}>
                         I am an accomplished Software Engineer with more than 3-year track record, primarily within the dynamic entertainment sector. My expertise lies in front-end development utilizing NextJS and React, as well as back-end proficiency with NodeJs and Python. In addition, I possess a strong command of database technologies including MySQL and MongoDB.
                     </ContentLabel>
